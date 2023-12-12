4313787b77984ff82ea5b64dcf3bd85ca3db3314
Kaustuv Karki <karkikaustuv@gmail.com>
2023-12-12 12:36:56 +0545

    Controllers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
@@ -13,3 +14,6 @@ mongoose
 app.listen(3030, () => {
   console.log("Server on port 3030");
 });
+
+app.use(express.json());
+app.use("/api/user", userRouter);
