31c80721775a559a9326ce6295bc6bb04f09b310
Kaustuv Karki <karkikaustuv@gmail.com>
2023-12-12 11:44:53 +0545

    Mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
 app.listen(3030, () => {
   console.log("Server on port 3030");
