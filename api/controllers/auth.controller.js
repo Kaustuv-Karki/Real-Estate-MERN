d18b8ec013cfc8ef97f01361f86a1ec7e2f0d0f8
Kaustuv Karki <karkikaustuv@gmail.com>
2023-12-12 19:01:35 +0545

    Middleware for errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 12);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -9,6 +9,6 @@ export const register = async (req, res) => {
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
