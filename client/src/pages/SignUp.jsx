76eadf520a753f035faef5ec6cef13d6828b62cc
Kaustuv Karki <karkikaustuv@gmail.com>
2023-12-13 23:11:25 +0545

    User information in frontend

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [userInput, setUserInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+  };
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-[3rem] ">Register</h1>
       <form className="flex flex-col w-[80vw] max-w-[40rem] gap-4">
         <input
           className=" border p-3 rounded-md outline-none"
+          name="username"
           type="text"
+          value={userInput.username}
+          onChange={(e) => handleChange(e)}
           placeholder="Username"
         />
 
         <input
           className=" border p-3 rounded-md outline-none"
           type="email"
+          name="email"
+          value={userInput.email}
+          onChange={(e) => handleChange(e)}
           placeholder="Email"
         />
 
         <input
           className=" border p-3 rounded-md outline-none"
           type="password"
+          value={userInput.password}
+          name="password"
+          onChange={(e) => handleChange(e)}
           placeholder="Password"
         />
         <button
