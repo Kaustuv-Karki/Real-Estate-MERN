d8595f37aedde77c8b4de9c47d27493acad5ea55
Kaustuv Karki <karkikaustuv@gmail.com>
2023-12-30 15:49:15 +0545

    Made user information update route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,33 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcryptjs";
+
 export const test = (req, res) => {
   res.send("This is the response from the user controller");
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(403, "You can not access the account"));
+  try {
+    if (req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 12);
+    }
+    const updateduser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          avatar: req.body.avatar,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...others } = updateduser._doc;
+    res.status(200).json({ message: "User updated successfully", others });
+  } catch (error) {
+    next(error);
+  }
 };
