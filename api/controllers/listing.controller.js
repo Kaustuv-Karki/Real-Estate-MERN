83a6c52780b4b0028bb05b3b96f867b0623c84e8
Kaustuv Karki <karkikaustuv@gmail.com>
2024-03-09 22:47:16 +0545

    Update contoller and route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -17,6 +18,23 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: "Listing was deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(res.params.id);
+  if (!listing) return next(errorHandler(404, "Listing Not Found"));
+  if (listing.userRef !== req.user.id)
+    return next(errorHandler(403, "You can not update this listing"));
+  try {
+    const updatedListing = await listing.findByIdAndUpdate(
+      req.params,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
